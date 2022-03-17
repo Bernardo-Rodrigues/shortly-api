f681812fcb5ca06fd77981095f9d7f8416380a90
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-03-17 20:32:35 -0300

    feat: partially add route to user information

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -25,11 +25,46 @@ export async function createUser(req, res) {
   }
 }
 
-export async function getUser(req, res) {
+export async function getUsers(req, res) {
   const { user } = res.locals;
 
   try {
     res.send(user);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function getUser(req, res) {
+  const { id } = req.params;
+
+  try {
+    let { rows: user} = await connection.query(`
+        SELECT  users.id, users.name,
+                SUM("visitCount") AS "visitCount",
+                urls.id AS "urlId", urls."shortUrl", urls.url, urls."visitCount" AS "urlVisitCount"
+          FROM  users 
+          JOIN  urls
+            ON  urls."userId" = users.id
+          WHERE  users.id = $1
+      GROUP BY  urls.id, users.id
+    `, [id]);
+
+    user = {
+      id:user[0].id,
+      name:user[0].name,
+      visitCount:user[0].visitCount,
+      shortenedUrls:user.map(url=> ({
+        id:url.urlId,
+        shortUrl:url.shortUrl,
+        url:url.url,
+        visitCount:url.urlVisitCount
+      }))
+    }
+
+    if (!user) return res.sendStatus(404)
+    res.send(user)
   } catch (error) {
     console.log(error);
     return res.sendStatus(500);
