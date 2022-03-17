ab2a3b26291e0888029bba796f48337ad634e64a
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-03-17 18:53:21 -0300

    feat: add post route to shorten url

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -3,14 +3,15 @@ import { v4 as uuid } from 'uuid';
 
 export async function createShortenUrl(req, res) {
     const { url } = req.body
+    const userId = res.locals.user.id
 
     try {
-        const shortUrl = uuid();
+        const shortUrl = uuid().split("-")[0];
 
         const result = await connection.query(`
-            INSERT INTO  urls ("shortUrl", url)
-                 VALUES  ($1, $2)
-        `, [shortUrl, url])
+            INSERT INTO  urls ("userId", "shortUrl", url)
+                 VALUES  ($1, $2, $3)
+        `, [userId ,shortUrl, url])
 
         if (!result.rowCount) throw new Error();
 
